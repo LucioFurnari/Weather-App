59080115c00b43105adfb6378f18d5c6463df9fb
LucioFurnari <luciofurnari99@gmail.com>
2023-02-07 19:57:17 -0300

    Add form and input in the weather card, create the loading component

--- src/components/uiComponents.js
+++ src/components/uiComponents.js
@@ -1,16 +1,28 @@
-function weatherCard() {
+export function weatherCard() {
   const card = document.createElement('div');
   const weather = document.createElement('p');
   const mainTemperature = document.createElement('p');
+  const cityForm = document.createElement('form');
+  const cityInput = document.createElement('input');
+  const submitButton = document.createElement('button');
 
   card.classList.add('card');
   weather.classList.add('weather');
   mainTemperature.classList.add('main-temperature');
+  cityForm.classList.add('city-form');
+  cityInput.classList.add('city-input');
 
-  card.append(weather, mainTemperature);
+  submitButton.textContent = 'Submit';
+
+  cityForm.append(cityInput, submitButton);
+  card.append(cityForm, weather, mainTemperature);
 
   const root = document.querySelector('#root');
   root.append(card);
 }
 
-export default weatherCard;
+export function loadingComponent() {
+  const loading = document.createElement('p');
+  loading.textContent = 'Loading...';
+  return loading;
+}
