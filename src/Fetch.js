3f7a116736f1cd3a1914e6bea567167195a43342
LucioFurnari <luciofurnari99@gmail.com>
2023-02-07 17:35:21 -0300

    Update the fetch function to retunr console log of error, and return false at the end

--- src/Fetch.js
+++ src/Fetch.js
@@ -1,12 +1,13 @@
-let APIKey = '4bdda9b7b6a4d3a1dd20a4a879e28e91';
+const APIKey = '4bdda9b7b6a4d3a1dd20a4a879e28e91';
 
 async function fetchAPI(location) {
   try {
-    let result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIKey}&units=metric`)
-    let data = await result.json();
-    return data
+    const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIKey}&units=metric`);
+    const data = await result.json();
+    return data;
   } catch (err) {
     console.log(err);
   }
+  return false;
 }
-export {fetchAPI}
+export default fetchAPI;
