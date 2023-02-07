6e6c450c5e8308799c72c76f53ef377f05b76243
LucioFurnari <luciofurnari99@gmail.com>
2023-02-07 18:54:05 -0300

    Add submit event to get the data of news citys

--- src/components/Control.js
+++ src/components/Control.js
@@ -5,16 +5,32 @@ async function controlEvents() {
   const loading = document.createElement('p');
   const weather = document.querySelector('.weather');
   const mainTemperature = document.querySelector('.main-temperature');
+  const cityForm = document.querySelector('.city-form');
 
   loading.textContent = 'Loading...';
   card.append(loading);
-  const data = await getData('41414141');
+  const data = await getData('Pergamino');
   if (data) {
     card.removeChild(loading);
     weather.textContent = data.weather;
     mainTemperature.textContent = data.temp;
   }
   loading.textContent = 'ERROR';
+
+  cityForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const cityInput = document.querySelector('.city-input');
+    const city = cityInput.value;
+    loading.textContent = 'Loading...';
+    card.append(loading);
+    const newData = await getData(city);
+    if (newData) {
+      card.removeChild(loading);
+      weather.textContent = newData.weather;
+      mainTemperature.textContent = newData.temp;
+    }
+    loading.textContent = 'ERROR';
+  });
 }
 
 export default controlEvents;
