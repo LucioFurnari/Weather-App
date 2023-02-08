63f9eebaf53f8644cdd3e4633bde8c96f57a737e
LucioFurnari <luciofurnari99@gmail.com>
2023-02-08 20:53:03 -0300

    Import bgClass.css file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { weatherCard } from './components/uiComponents';
 import controlEvents from './components/Control';
 import './styles/style.css';
+import './styles/bgClass.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   weatherCard();
