ecdda3425f2bbfecb88ebfd297b6b236e4ee20fc
LucioFurnari <luciofurnari99@gmail.com>
2023-02-08 23:28:02 -0300

    Create city, country names components

--- src/components/uiComponents.js
+++ src/components/uiComponents.js
@@ -3,20 +3,24 @@ import temperatureSection from './tempComponent';
 export function weatherCard() {
   const card = document.createElement('div');
   const weather = document.createElement('p');
+  const cityName = document.createElement('p');
+  const countryName = document.createElement('p');
   const cityForm = document.createElement('form');
   const cityInput = document.createElement('input');
   const submitButton = document.createElement('button');
   const tempSection = temperatureSection();
 
   card.classList.add('card');
-  weather.classList.add('weather');
-  cityForm.classList.add('city-form');
-  cityInput.classList.add('city-input');
+  weather.classList.add('card-weather');
+  cityName.classList.add('card-name');
+  countryName.classList.add('card-country');
+  cityForm.classList.add('card-form');
+  cityInput.classList.add('card-input');
 
   submitButton.textContent = 'Submit';
 
   cityForm.append(cityInput, submitButton);
-  card.append(cityForm, weather, tempSection);
+  card.append(cityForm, cityName, countryName, weather, tempSection);
 
   const root = document.querySelector('#root');
   root.append(card);
