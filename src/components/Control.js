81d19be8ca5666b2e66274d25a772a83e1c2b09f
LucioFurnari <luciofurnari99@gmail.com>
2023-02-08 23:27:32 -0300

    Call city, country card components and they values

--- src/components/Control.js
+++ src/components/Control.js
@@ -4,33 +4,44 @@ import { loadingComponent } from './uiComponents';
 
 async function controlEvents() {
   const card = document.querySelector('.card');
-  const weather = document.querySelector('.weather');
+  const cardWeather = document.querySelector('.card-weather');
+  const cityName = document.querySelector('.card-name');
+  const countryName = document.querySelector('.card-country');
   const currentTemp = document.querySelector('.main-temperature');
   // const minTemp = document.querySelector('.min-temp');
   // const maxTemp = document.querySelector('.max-temp');
-  const cityForm = document.querySelector('.city-form');
+  const cityForm = document.querySelector('.card-form');
   const loading = loadingComponent();
 
   card.append(loading);
   const data = await getData('Pergamino');
   if (data) {
+    const {
+      city,
+      country,
+      weather,
+      temp,
+    } = data;
+
     card.removeChild(loading);
-    weather.textContent = data.weather;
-    currentTemp.textContent = `${data.temp} C°`;
+    cityName.textContent = city;
+    countryName.textContent = country;
+    cardWeather.textContent = weather;
+    currentTemp.textContent = `${temp} C°`;
 
     changeBackground(data.weather);
   }
 
   cityForm.addEventListener('submit', async (e) => {
     e.preventDefault();
-    const cityInput = document.querySelector('.city-input');
+    const cityInput = document.querySelector('.card-input');
     const city = cityInput.value;
 
     card.append(loading);
     const newData = await getData(city);
     if (newData) {
       card.removeChild(loading);
-      weather.textContent = newData.weather;
+      cardWeather.textContent = newData.weather;
       currentTemp.textContent = newData.temp;
 
       changeBackground(newData.weather);
