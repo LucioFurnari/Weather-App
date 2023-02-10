1d9197663503b3bd7c7142b3089614e2360c6ad2
LucioFurnari <luciofurnari99@gmail.com>
2023-02-10 14:40:56 -0300

    Separate the code in function, and the form event from the setUi function

--- src/components/Control.js
+++ src/components/Control.js
@@ -2,19 +2,17 @@ import getData from '../Data';
 import changeBackground from './changeBackground';
 import { loadingComponent } from './uiComponents';
 
-async function controlEvents() {
+async function setUi(location = 'Pergamino') {
   const card = document.querySelector('.card');
   const cardWeather = document.querySelector('.card-weather');
   const cityName = document.querySelector('.card-name');
   const countryName = document.querySelector('.card-country');
   const currentTemp = document.querySelector('.main-temperature');
-  // const minTemp = document.querySelector('.min-temp');
-  // const maxTemp = document.querySelector('.max-temp');
-  const cityForm = document.querySelector('.card-form');
-  const loading = loadingComponent();
 
+  const loading = loadingComponent();
   card.append(loading);
-  const data = await getData('Pergamino');
+
+  const data = await getData(location);
   if (data) {
     const {
       city,
@@ -22,8 +20,8 @@ async function controlEvents() {
       weather,
       temp,
     } = data;
-
     card.removeChild(loading);
+
     cityName.textContent = city;
     countryName.textContent = country;
     cardWeather.textContent = weather;
@@ -31,22 +29,17 @@ async function controlEvents() {
 
     changeBackground(data.weather);
   }
-
-  cityForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const cityInput = document.querySelector('.card-input');
-    const city = cityInput.value;
-
-    card.append(loading);
-    const newData = await getData(city);
-    if (newData) {
-      card.removeChild(loading);
-      cardWeather.textContent = newData.weather;
-      currentTemp.textContent = newData.temp;
-
-      changeBackground(newData.weather);
-    }
-  });
 }
 
-export default controlEvents;
+async function searchCity(e) {
+  const cardInput = document.querySelector('.card-input');
+  e.preventDefault();
+  setUi(cardInput.value);
+}
+
+function loadUi() {
+  setUi();
+  const cityForm = document.querySelector('.card-form');
+  cityForm.addEventListener('submit', searchCity);
+}
+export default loadUi;
