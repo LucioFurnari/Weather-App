c023ec2034a902bbbea1874315f283f4d8fce7a0
LucioFurnari <luciofurnari99@gmail.com>
2023-02-10 16:39:24 -0300

    Fix and remove point in class name

--- src/components/tempComponent.js
+++ src/components/tempComponent.js
@@ -5,7 +5,7 @@ function temperatureSection() {
   // const minTemperature = document.createElement('span');
   // const maxTemperature = document.createElement('span');
 
-  tempSection.classList.add('.temp-section');
+  tempSection.classList.add('temp-section');
   currentTemperature.classList.add('main-temperature');
   // minTemperature.classList.add('min-temp');
   // maxTemperature.classList.add('max-temp');
