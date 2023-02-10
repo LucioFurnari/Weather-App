ecef2c7b0c092e756239802707e52102533e5316
LucioFurnari <luciofurnari99@gmail.com>
2023-02-10 16:39:59 -0300

    Import and add city component

--- src/components/uiComponents.js
+++ src/components/uiComponents.js
@@ -1,26 +1,24 @@
 import temperatureSection from './tempComponent';
+import cityComponent from './namesComponent';
 
 export function weatherCard() {
   const card = document.createElement('div');
   const weather = document.createElement('p');
-  const cityName = document.createElement('p');
-  const countryName = document.createElement('p');
   const cityForm = document.createElement('form');
   const cityInput = document.createElement('input');
   const submitButton = document.createElement('button');
   const tempSection = temperatureSection();
+  const citySection = cityComponent();
 
   card.classList.add('card');
   weather.classList.add('card-weather');
-  cityName.classList.add('card-name');
-  countryName.classList.add('card-country');
   cityForm.classList.add('card-form');
   cityInput.classList.add('card-input');
 
   submitButton.textContent = 'Submit';
 
   cityForm.append(cityInput, submitButton);
-  card.append(cityForm, cityName, countryName, weather, tempSection);
+  card.append(cityForm, weather, citySection, tempSection);
 
   const root = document.querySelector('#root');
   root.append(card);
