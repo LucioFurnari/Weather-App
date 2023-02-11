d64830c7b532e1aa996269d85badb2a0a913a71c
LucioFurnari <luciofurnari99@gmail.com>
2023-02-11 17:24:26 -0300

    Change the component to separate the conditions in sections

--- src/components/conditionsComponent.js
+++ src/components/conditionsComponent.js
@@ -1,14 +1,20 @@
 function conditionsComponent() {
-  const humidity = document.createElement('p');
-  // const windDeg = document.createElement('p');
-  // const windSpeed = document.createElement('p');
-  humidity.classList.add('card-humidity');
+  const humiditySection = document.createElement('div');
+  const humidityPercentage = document.createElement('span');
   const humiditySvg = document.createElement('img');
+  const windDeg = document.createElement('p');
+  const windSvg = document.createElement('img');
+  const windSpeed = document.createElement('p');
+
+  humiditySection.classList.add('card-humidity');
+
   humiditySvg.src = '../src/images/humidity.svg';
+  windSvg.src = '../src/images/wind.svg';
 
-  humidity.append(humiditySvg);
+  humidityPercentage.innerText = '40%';
+  humiditySection.append(humidityPercentage, humiditySvg);
 
-  return humidity;
+  return humiditySection;
 }
 
 export default conditionsComponent;
