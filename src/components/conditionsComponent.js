c98047467b7ef0ca68bb29a14c75ac3ac9c39f6e
LucioFurnari <luciofurnari99@gmail.com>
2023-02-11 17:26:13 -0300

    Add wind section, and class row to the components

--- src/components/conditionsComponent.js
+++ src/components/conditionsComponent.js
@@ -2,17 +2,20 @@ function conditionsComponent() {
   const humiditySection = document.createElement('div');
   const humidityPercentage = document.createElement('span');
   const humiditySvg = document.createElement('img');
-  const windDeg = document.createElement('p');
+  const windSection = document.createElement('div');
+  const windDeg = document.createElement('span');
   const windSvg = document.createElement('img');
-  const windSpeed = document.createElement('p');
+  const windSpeed = document.createElement('span');
 
-  humiditySection.classList.add('card-humidity');
+  humiditySection.classList.add('card-humidity', 'row');
+  windSection.classList.add('card-wind', 'row');
 
   humiditySvg.src = '../src/images/humidity.svg';
   windSvg.src = '../src/images/wind.svg';
 
   humidityPercentage.innerText = '40%';
   humiditySection.append(humidityPercentage, humiditySvg);
+  windSection.append(windDeg, windSvg);
 
   return humiditySection;
 }
