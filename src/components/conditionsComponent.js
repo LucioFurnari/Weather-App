c4b8f5a38d7ade7986bc8a3ffb7f22cc5bd35c84
LucioFurnari <luciofurnari99@gmail.com>
2023-02-14 15:43:54 -0300

    Remove wind deg and add wind speed, add class to the two elements

--- src/components/conditionsComponent.js
+++ src/components/conditionsComponent.js
@@ -4,20 +4,20 @@ function conditionsComponent() {
   const humidityPercentage = document.createElement('span');
   const humiditySvg = document.createElement('img');
   const windSection = document.createElement('div');
-  const windDeg = document.createElement('span');
   const windSvg = document.createElement('img');
   const windSpeed = document.createElement('span');
 
   conditionsSection.classList.add('condition-section', 'row');
-  humiditySection.classList.add('card-humidity', 'row');
+  humiditySection.classList.add('humidity-section', 'row');
   windSection.classList.add('card-wind', 'row');
+  humidityPercentage.classList.add('card-humidity');
+  windSpeed.classList.add('card-speed');
 
   humiditySvg.src = '../src/images/humidity.svg';
   windSvg.src = '../src/images/wind.svg';
 
-  humidityPercentage.innerText = '40%';
   humiditySection.append(humidityPercentage, humiditySvg);
-  windSection.append(windDeg, windSvg);
+  windSection.append(windSpeed, windSvg);
   conditionsSection.append(humiditySection, windSection);
 
   return conditionsSection;
