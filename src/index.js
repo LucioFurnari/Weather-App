7d61652019bac8d4ccbecec95a2057e5f9053fca
LucioFurnari <luciofurnari99@gmail.com>
2023-02-14 16:12:59 -0300

    Typo fix for the imported functions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import { weatherCard } from './components/uiComponents';
-import loadUi from './components/Control';
+import { weatherCard } from './components/cardComponent';
+import loadUiContent from './components/Control';
 import './styles/style.css';
 import './styles/bgClass.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   weatherCard();
-  loadUi();
+  loadUiContent();
 });
