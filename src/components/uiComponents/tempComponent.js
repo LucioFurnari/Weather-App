f6290380be7d191c3acfea1f7f705af4164f564a
LucioFurnari <luciofurnari99@gmail.com>
2023-02-16 23:48:06 -0300

    Add min and max temperature

--- src/components/uiComponents/tempComponent.js
+++ src/components/uiComponents/tempComponent.js
@@ -1,17 +1,19 @@
-function temperatureSection() {
+function temperatureSection(max, min) {
   const tempSection = document.createElement('div');
-  // const tempSeparation = document.createElement('div');
+  const tempSeparation = document.createElement('div');
   const currentTemperature = document.createElement('span');
-  // const minTemperature = document.createElement('span');
-  // const maxTemperature = document.createElement('span');
+  const minTemperature = document.createElement('span');
+  const maxTemperature = document.createElement('span');
 
   tempSection.classList.add('temp-section');
   currentTemperature.classList.add('main-temperature');
-  // minTemperature.classList.add('min-temp');
-  // maxTemperature.classList.add('max-temp');
+  maxTemperature.textContent = max;
+  minTemperature.textContent = min;
+  minTemperature.classList.add('min-temp');
+  maxTemperature.classList.add('max-temp');
 
-  // tempSeparation.append(minTemperature, maxTemperature);
-  tempSection.append(currentTemperature);
+  tempSeparation.append(minTemperature, maxTemperature);
+  tempSection.append(currentTemperature, tempSeparation);
   return tempSection;
 }
 
