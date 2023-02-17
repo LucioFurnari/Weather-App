bb37944f9b7b513e8322ef7a2cbb7f8dd3305717
LucioFurnari <luciofurnari99@gmail.com>
2023-02-16 22:30:08 -0300

    Change the function to recive the new data

--- src/components/Control.js
+++ src/components/Control.js
@@ -1,34 +1,46 @@
 import getData from '../Data';
 import changeBackground from './changeBackground';
 import loadingComponent from './uiComponents/loadingComponent';
+import weatherCard from './uiComponents/cardComponent';
 
 async function setUiContent(location = 'Pergamino') {
-  const card = document.querySelector('.card');
-  const cardWeather = document.querySelector('.card-weather');
-  const cityName = document.querySelector('.card-name');
-  const countryName = document.querySelector('.card-country');
-  const currentTemp = document.querySelector('.main-temperature');
+  const main = document.querySelector('main');
 
-  const loading = loadingComponent();
-  card.append(loading);
-  card.classList.remove('loaded');
   const data = await getData(location);
   if (data) {
-    card.classList.add('loaded');
     const {
-      city,
-      country,
-      weather,
-      temp,
+      actualTemperature,
+      apparentTemperature,
+      weatherCode,
+      windDirection,
+      windSpeed,
+      dailyTime,
+      dailyTempMax,
+      dailyTempMin,
+      dailyWeatherCode,
+      listOfDays,
+      dayOfWeek
     } = data;
-    card.removeChild(loading);
+    changeBackground(dailyWeatherCode[0]);
+    dailyTime.map((day) => {
+      const card = weatherCard();
+      card.classList.add('loaded');
+      main.append(card);
+    });
+    // const card = weatherCard();
+    // main.append(card);
+    // const cardWeather = document.querySelector('.card-weather');
+    // const cityName = document.querySelector('.card-name');
+    // const countryName = document.querySelector('.card-country');
+    // const currentTemp = document.querySelector('.main-temperature');
+    // card.classList.add('loaded');
+    // // card.removeChild(loading);
 
-    cityName.textContent = city;
-    countryName.textContent = country;
-    cardWeather.textContent = weather;
-    currentTemp.textContent = `${temp} C°`;
+    // cityName.textContent = city;
+    // countryName.textContent = country;
+    // cardWeather.textContent = weather;
+    // currentTemp.textContent = `${temp} C°`;
 
-    changeBackground(data.weather);
   }
 }
 
