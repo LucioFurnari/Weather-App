fc57cfec985156190ccc2085caeb735cef7fd1dd
LucioFurnari <luciofurnari99@gmail.com>
2023-02-16 22:28:19 -0300

    Create functions to get the day of the week for each day returned from the api

--- src/Data.js
+++ src/Data.js
@@ -1,4 +1,4 @@
-import { fetchAPI, newFetch } from './Fetch';
+import { fetchAPI } from './Fetch';
 
 function getHours(time) {
   const actualDate = new Date().getHours();
@@ -11,8 +11,49 @@ function getHours(time) {
   });
   return index;
 }
-async function newGetData(location) {
-  const data = await newFetch(location);
+function getDays(date) {
+  const listOfDays = [];
+  const actualHour = new Date().getHours();
+  date.map(day => {
+    const newDate = new Date(`${day} ${actualHour}:00:00`);
+    const newDay = newDate.getDay();
+    listOfDays.push(newDay);
+  });
+  return listOfDays;
+}
+function dayOfTheWeek(date) {
+  const dayOfWeek = [];
+  date.map((day) => {
+    switch (day) {
+      case 0:
+        dayOfWeek.push('Sunday');
+        break;
+      case 1:
+        dayOfWeek.push('Monday');
+        break;
+      case 2:
+        dayOfWeek.push('Tuesday');
+        break;
+      case 3:
+        dayOfWeek.push('Wednesday');
+        break;
+      case 4:
+        dayOfWeek.push('Thursday');
+        break;
+      case 5:
+        dayOfWeek.push('Friday');
+        break;
+      case 6:
+        dayOfWeek.push('Saturday');
+        break;
+      default:
+        break;
+    }
+  });
+  return dayOfWeek;
+}
+async function getData(location) {
+  const data = await fetchAPI(location);
   const {
     time,
     temperature_2m: actualTemperature,
@@ -27,7 +68,10 @@ async function newGetData(location) {
     temperature_2m_min: dailyTempMin,
     weathercode: dailyWeatherCode,
   } = data.daily;
+
   const actualTime = getHours(time);
+  const listOfDays = getDays(dailyTime);
+  const dayOfWeek = dayOfTheWeek(listOfDays);
 
   const newData = {
     actualTemperature: actualTemperature[actualTime],
@@ -39,38 +83,40 @@ async function newGetData(location) {
     dailyTempMax,
     dailyTempMin,
     dailyWeatherCode,
+    listOfDays,
+    dayOfWeek,
   };
   console.log(newData);
+  return newData;
 }
-newGetData('Pergamino');
 
-async function getData(location) {
-  const data = await fetchAPI(location);
-  if (data.cod !== '404' && data.cod !== '400') {
-    const [{ main: weather, description: weatherDescription }] = data.weather;
-    const { deg: windDeg, speed: windSpeed } = data.wind;
-    const {
-      temp,
-      temp_max: tempMax,
-      temp_min: tempMin,
-      humidity,
-    } = data.main;
-    const newData = {
-      weather,
-      temp,
-      tempMax,
-      tempMin,
-      humidity,
-      windDeg,
-      windSpeed,
-      weatherDescription,
-      timezone: new Date(data.timezone * 1000).toISOString().slice(11, 19),
-      city: data.name, // Check for the Geocoding API to get the exact location
-      country: data.sys.country, // Check for the Geocoding API to get the exact location
-    };
-    return newData;
-  }
-  return false;
-}
+// async function getData(location) {
+//   const data = await fetchAPI(location);
+//   if (data.cod !== '404' && data.cod !== '400') {
+//     const [{ main: weather, description: weatherDescription }] = data.weather;
+//     const { deg: windDeg, speed: windSpeed } = data.wind;
+//     const {
+//       temp,
+//       temp_max: tempMax,
+//       temp_min: tempMin,
+//       humidity,
+//     } = data.main;
+//     const newData = {
+//       weather,
+//       temp,
+//       tempMax,
+//       tempMin,
+//       humidity,
+//       windDeg,
+//       windSpeed,
+//       weatherDescription,
+//       timezone: new Date(data.timezone * 1000).toISOString().slice(11, 19),
+//       city: data.name, // Check for the Geocoding API to get the exact location
+//       country: data.sys.country, // Check for the Geocoding API to get the exact location
+//     };
+//     return newData;
+//   }
+//   return false;
+// }
 
 export default getData;
