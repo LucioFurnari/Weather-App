d81ad938c4f36dc0e93dc72362eaea6a0071889a
LucioFurnari <luciofurnari99@gmail.com>
2023-02-16 22:31:33 -0300

    Change the api

--- src/Fetch.js
+++ src/Fetch.js
@@ -1,15 +1,15 @@
 const APIKey = '4bdda9b7b6a4d3a1dd20a4a879e28e91';
 
-export async function fetchAPI(location) {
-  try {
-    const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIKey}&units=metric`);
-    const data = await result.json();
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-  return false;
-}
+// export async function fetchAPI(location) {
+//   try {
+//     const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIKey}&units=metric`);
+//     const data = await result.json();
+//     return data;
+//   } catch (err) {
+//     console.log(err);
+//   }
+//   return false;
+// }
 
 // Check for the Geocoding API to get the exact location
 
@@ -24,7 +24,7 @@ export async function fetchGeoApi(location) {
   return false;
 }
 
-export async function newFetch(location) {
+export async function fetchAPI(location) {
   const [{ lat, lon }] = await fetchGeoApi(location);
   const result = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,weathercode,windspeed_10m,winddirection_10m&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=auto`);
   const data = await result.json();
