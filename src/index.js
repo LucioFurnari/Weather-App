e464ac31668ed89ae16748c3e4dceaa676f33320
LucioFurnari <luciofurnari99@gmail.com>
2023-02-16 22:31:57 -0300

    Add main section and remove weather card

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import weatherCard from './components/cardComponent';
 import loadUiContent from './components/Control';
+import mainSection from './components/mainSection';
 import './styles/style.css';
 import './styles/bgClass.css';
 
 document.addEventListener('DOMContentLoaded', () => {
-  weatherCard();
+  mainSection();
   loadUiContent();
 });
