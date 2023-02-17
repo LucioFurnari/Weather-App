409c872fa37430312f0a9ca1913352eb3d9d9340
LucioFurnari <luciofurnari99@gmail.com>
2023-02-17 16:56:13 -0300

    Prevent the fetch to return empy data

--- src/Fetch.js
+++ src/Fetch.js
@@ -1,18 +1,5 @@
 const APIKey = '4bdda9b7b6a4d3a1dd20a4a879e28e91';
 
-// export async function fetchAPI(location) {
-//   try {
-//     const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIKey}&units=metric`);
-//     const data = await result.json();
-//     return data;
-//   } catch (err) {
-//     console.log(err);
-//   }
-//   return false;
-// }
-
-// Check for the Geocoding API to get the exact location
-
 export async function fetchGeoApi(location) {
   try {
     const result = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${APIKey}`);
@@ -25,9 +12,16 @@ export async function fetchGeoApi(location) {
 }
 
 export async function fetchAPI(location) {
-  const [{ lat, lon }] = await fetchGeoApi(location);
-  const result = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,weathercode,windspeed_10m,winddirection_10m&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=auto`);
-  const data = await result.json();
-  console.log(data);
-  return data;
+  const geoData = await fetchGeoApi(location);
+  if (geoData.length > 0 && geoData.cod !== '400') {
+    const [{ lat, lon }] = geoData;
+    try {
+      const result = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,weathercode,windspeed_10m,winddirection_10m&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=auto`);
+      const data = await result.json();
+      return data;
+    } catch (err) {
+      return err;
+    }
+  }
+  return false;
 }
