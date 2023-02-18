8358773f23a1d3e20985af318e60aa5964205681
LucioFurnari <luciofurnari99@gmail.com>
2023-02-18 20:38:59 -0300

    Add function to return weather depending of the code of the api

--- src/components/uiComponents/cardComponent.js
+++ src/components/uiComponents/cardComponent.js
@@ -2,6 +2,57 @@ import temperatureSection from './tempComponent';
 import cityComponent from './namesComponent';
 import conditionsComponent from './conditionsComponent';
 
+function getWeather(code) {
+  let weather;
+  switch (code) {
+    case 0:
+      weather = 'Clear';
+      break;
+    case 1:
+    case 2:
+    case 3:
+      weather = 'Clouds';
+      break;
+    case 61:
+    case 63:
+    case 65:
+    case 66:
+    case 67:
+    case 80:
+    case 81:
+    case 82:
+      weather = 'Rain';
+      break;
+    case 95:
+    case 96:
+    case 99:
+      weather = 'Thunder';
+      break;
+    case 45:
+    case 48:
+      weather = 'Mist';
+      break;
+    case 51:
+    case 53:
+    case 55:
+    case 56:
+    case 57:
+      weather = 'Drizzle';
+      break;
+    case 71:
+    case 73:
+    case 75:
+    case 77:
+    case 85:
+    case 86:
+      weather = 'Snow';
+      break;
+    default:
+      break;
+  }
+  return weather;
+}
+
 function weatherCard(props) {
   const {
     country,
@@ -30,6 +81,8 @@ function weatherCard(props) {
   card.classList.add('card');
   dayOfWeek.textContent = `${dayOfTheWeek} ${dayNumber} ${month}`;
   weather.classList.add('card-weather');
+
+  weather.textContent = getWeather(weatherCode);
 
   card.append(dayOfWeek, weather, citySection, tempSection, conditionSection);
 
