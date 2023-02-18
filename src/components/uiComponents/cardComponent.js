2ea16ea685d389030ac53148a2eea74e068b3de1
LucioFurnari <luciofurnari99@gmail.com>
2023-02-18 16:16:22 -0300

    Add wind speed, direction, humidity

--- src/components/uiComponents/cardComponent.js
+++ src/components/uiComponents/cardComponent.js
@@ -4,20 +4,28 @@ import conditionsComponent from './conditionsComponent';
 
 function weatherCard(props) {
   const {
+    country,
+    name,
+    state,
     dayOfTheWeek,
     dayNumber,
     month,
     tempMax,
     tempMin,
     actualTemperature,
+    apparentTemperature,
+    weatherCode,
+    windDirection,
+    windSpeed,
+    humidity,
   } = props;
   const card = document.createElement('div');
   const dayOfWeek = document.createElement('p');
   const weather = document.createElement('p');
 
   const tempSection = temperatureSection(actualTemperature, tempMax, tempMin);
-  const citySection = cityComponent();
-  const conditionSection = conditionsComponent();
+  const citySection = cityComponent(name, country);
+  const conditionSection = conditionsComponent(humidity, windSpeed, windDirection);
 
   card.classList.add('card');
   dayOfWeek.textContent = `${dayOfTheWeek} ${dayNumber} ${month}`;
