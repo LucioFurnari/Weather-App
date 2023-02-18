7f4adf28398c675f6c1f3f58d45b05ba47572ce0
LucioFurnari <luciofurnari99@gmail.com>
2023-02-18 16:21:15 -0300

    Change each case to take numbers

--- src/components/changeBackground.js
+++ src/components/changeBackground.js
@@ -1,32 +1,53 @@
 function changeBackground(weather) {
   const root = document.querySelector('#root');
-  const currentClass = (root.className === '') ? 'class' : root.className;
+  const currentClass = (root.className === '') ? 'empty' : root.className;
   switch (weather) {
-    case 'Clear':
+    case 0:
       root.classList.remove(currentClass);
       root.classList.add('clear-bg');
       break;
-    case 'Clouds':
+    case 1:
+    case 2:
+    case 3:
       root.classList.remove(currentClass);
       root.classList.add('cloud-bg');
       break;
-    case 'Rain':
+    case 61:
+    case 63:
+    case 65:
+    case 66:
+    case 67:
+    case 80:
+    case 81:
+    case 82:
       root.classList.remove(currentClass);
       root.classList.add('rain-bg');
       break;
-    case 'Thunderstorm':
+    case 95:
+    case 96:
+    case 99:
       root.classList.remove(currentClass);
       root.classList.add('thunder-bg');
       break;
-    case 'Mist':
+    case 45:
+    case 48:
       root.classList.remove(currentClass);
       root.classList.add('mist-bg');
       break;
-    case 'Drizzle':
+    case 51:
+    case 53:
+    case 55:
+    case 56:
+    case 57:
       root.classList.remove(currentClass);
       root.classList.add('drizzle-bg');
       break;
-    case 'Snow':
+    case 71:
+    case 73:
+    case 75:
+    case 77:
+    case 85:
+    case 86:
       root.classList.remove(currentClass);
       root.classList.add('snow-bg');
       break;
