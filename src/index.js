4d5ff6beb6e4c7363c62e53b65e2f9cb810d05fc
LucioFurnari <luciofurnari99@gmail.com>
2023-02-18 18:44:17 -0300

    Import loading.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import loadUiContent from './components/Control';
 import mainSection from './components/mainSection';
 import './styles/style.css';
 import './styles/bgClass.css';
+import './styles/loading.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   mainSection();
