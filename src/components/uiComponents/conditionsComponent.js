086907e8f8df1efb19d9fe4bd37c6f54b477b6a9
LucioFurnari <luciofurnari99@gmail.com>
2023-02-18 16:19:36 -0300

    Add wind direction, speed, humidity, rotate the arrow of the wind depending of the direction

--- src/components/uiComponents/conditionsComponent.js
+++ src/components/uiComponents/conditionsComponent.js
@@ -1,23 +1,31 @@
-function conditionsComponent() {
+function conditionsComponent(humidity, windS, direction) {
   const conditionsSection = document.createElement('div');
   const humiditySection = document.createElement('div');
   const humidityPercentage = document.createElement('span');
+  const windSpeed = document.createElement('span');
+  const windDirection = document.createElement('img');
   const humiditySvg = document.createElement('img');
   const windSection = document.createElement('div');
   const windSvg = document.createElement('img');
-  const windSpeed = document.createElement('span');
 
   conditionsSection.classList.add('condition-section', 'row');
   humiditySection.classList.add('humidity-section', 'row');
   windSection.classList.add('card-wind', 'row');
   humidityPercentage.classList.add('card-humidity');
   windSpeed.classList.add('card-speed');
+  windDirection.classList.add('card-direction');
+
+  humidityPercentage.textContent = humidity;
+  windSpeed.textContent = windS;
 
   humiditySvg.src = '../src/images/humidity.svg';
   windSvg.src = '../src/images/wind.svg';
+  windDirection.src = '../src/images/arrow.svg';
+
+  windDirection.style = `transform: rotate(${direction}deg)`;
 
   humiditySection.append(humidityPercentage, humiditySvg);
-  windSection.append(windSpeed, windSvg);
+  windSection.append(windSpeed, windSvg, windDirection);
   conditionsSection.append(humiditySection, windSection);
 
   return conditionsSection;
