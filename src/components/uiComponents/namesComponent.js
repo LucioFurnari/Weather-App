118305f6ecf1c24e4d1b4b848db3fa42a789bff9
LucioFurnari <luciofurnari99@gmail.com>
2023-02-18 16:21:50 -0300

    Add city name and country code

--- src/components/uiComponents/namesComponent.js
+++ src/components/uiComponents/namesComponent.js
@@ -1,4 +1,4 @@
-function cityComponent() {
+function cityComponent(city, country) {
   const container = document.createElement('div');
   const cityName = document.createElement('p');
   const countryName = document.createElement('p');
@@ -6,6 +6,9 @@ function cityComponent() {
   container.classList.add('card-names-container', 'row');
   cityName.classList.add('card-name');
   countryName.classList.add('card-country');
+
+  cityName.textContent = city;
+  countryName.textContent = country;
 
   container.append(cityName, countryName);
 
