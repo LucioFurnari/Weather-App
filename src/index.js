809dc6a1d3c169de1876717ffa225203b88be903
LucioFurnari <luciofurnari99@gmail.com>
2023-02-19 13:57:17 -0300

    Import weather icon css file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import mainSection from './components/mainSection';
 import './styles/style.css';
 import './styles/bgClass.css';
 import './styles/loading.css';
+import './styles/weather-icons.min.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   mainSection();
