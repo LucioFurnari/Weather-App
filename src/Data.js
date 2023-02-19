f3bada3c4060a7a7a2aaf2bf992cd90cbf3b80db
LucioFurnari <luciofurnari99@gmail.com>
2023-02-19 20:07:32 -0300

    Import functions from dateFunctions

--- src/Data.js
+++ src/Data.js
@@ -1,128 +1,8 @@
 import { fetchAPI } from './Fetch';
+import {
+  getHours, getDays, getDate, getMonths, monthList, dayOfTheWeek,
+} from './dateFunctions';
 
-function getHours(time) {
-  const actualDate = new Date().getHours();
-  const list = [];
-  time.forEach((date, index) => {
-    const tempTime = new Date(date).getHours();
-    if (tempTime === actualDate) {
-      list.push(index);
-    }
-  });
-  return list;
-}
-function getDays(date) {
-  const listOfDays = [];
-  const actualHour = new Date().getHours();
-  date.map((day) => {
-    const newDate = new Date(`${day} ${actualHour}:00:00`);
-    const newDay = newDate.getDay();
-    listOfDays.push(newDay);
-    return false;
-  });
-  return listOfDays;
-}
-function getDate(date) {
-  const listOfDate = [];
-  const actualHour = new Date().getHours();
-  date.map((day) => {
-    const newDate = new Date(`${day} ${actualHour}:00:00`);
-    const numberDate = newDate.getDate();
-    listOfDate.push(numberDate);
-    return false;
-  });
-  return listOfDate;
-}
-function getMonths(date) {
-  const listOfMonths = [];
-  const actualHour = new Date().getHours();
-  date.map((day) => {
-    const newDate = new Date(`${day} ${actualHour}:00:00`);
-    const newDay = newDate.getMonth();
-    listOfMonths.push(newDay);
-    return false;
-  });
-  return listOfMonths;
-}
-function monthList(date) {
-  const months = [];
-  date.map((month) => {
-    switch (month) {
-      case 0:
-        months.push('January');
-        break;
-      case 1:
-        months.push('February');
-        break;
-      case 2:
-        months.push('March');
-        break;
-      case 3:
-        months.push('April');
-        break;
-      case 4:
-        months.push('May');
-        break;
-      case 5:
-        months.push('June');
-        break;
-      case 6:
-        months.push('July');
-        break;
-      case 7:
-        months.push('August');
-        break;
-      case 8:
-        months.push('September');
-        break;
-      case 9:
-        months.push('October');
-        break;
-      case 10:
-        months.push('November');
-        break;
-      case 11:
-        months.push('December');
-        break;
-      default:
-        break;
-    }
-    return false;
-  });
-  return months;
-}
-function dayOfTheWeek(date) {
-  const dayOfWeek = [];
-  date.map((day) => {
-    switch (day) {
-      case 0:
-        dayOfWeek.push('Sunday');
-        break;
-      case 1:
-        dayOfWeek.push('Monday');
-        break;
-      case 2:
-        dayOfWeek.push('Tuesday');
-        break;
-      case 3:
-        dayOfWeek.push('Wednesday');
-        break;
-      case 4:
-        dayOfWeek.push('Thursday');
-        break;
-      case 5:
-        dayOfWeek.push('Friday');
-        break;
-      case 6:
-        dayOfWeek.push('Saturday');
-        break;
-      default:
-        break;
-    }
-    return false;
-  });
-  return dayOfWeek;
-}
 function getActualData(arr, indexArr) {
   const newArr = [];
   arr.forEach((elem, index) => {
@@ -134,6 +14,7 @@ function getActualData(arr, indexArr) {
   });
   return newArr;
 }
+
 async function getData(location) {
   const returnedData = await fetchAPI(location);
   const { data, geoData } = returnedData;
