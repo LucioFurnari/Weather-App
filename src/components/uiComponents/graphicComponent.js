9870c8cad057f5e46546e2bb0c856f0688f5dfb5
LucioFurnari <luciofurnari99@gmail.com>
2023-02-21 23:10:29 -0300

    Remove fill of the svg

--- src/components/uiComponents/graphicComponent.js
+++ src/components/uiComponents/graphicComponent.js
@@ -1,9 +1,8 @@
 function drawSvgGraphic() {
   const graphic = document.createElement('div');
   graphic.innerHTML = `
-  <svg width="100" height="100">
-    <path d="M0 27 H 90 L 180 38" stroke="black"/>
-    <circle cx="0" cy="0" r="2" fill="red"/>
+  <svg width="100%" height="100">
+    <path d="M0 27 H 90 L 180 38 " stroke="black" fill="none"/>
   </svg>
   `;
   return graphic;
