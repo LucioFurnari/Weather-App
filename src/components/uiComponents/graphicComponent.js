59c890a036ec3a774e9d02791decfe1aa1740cf8
LucioFurnari <luciofurnari99@gmail.com>
2023-02-23 23:23:31 -0300

    Add temp number to the graphic

--- src/components/uiComponents/graphicComponent.js
+++ src/components/uiComponents/graphicComponent.js
@@ -2,24 +2,28 @@ const array = [27, 32, 38, 21, 27, 25, 24, 26];
 function createLines(arr) {
   let path = '';
   const point = 100;
+  const numberList = [];
   let line = 50;
   arr.forEach((elem) => {
     const temp = point - elem;
+    const text = `<text x=${line} y=${temp}>${elem}</text>`;
+    numberList.push(text);
     path += `${temp} L ${line} `;
     line += 50;
   });
-  return path;
+  return { path, numberList };
 }
 console.log(createLines(array));
 
 function drawSvgGraphic() {
-  const path = createLines(array);
+  const data = createLines(array);
   const graphic = document.createElement('div');
   graphic.classList.add('row');
   graphic.innerHTML = `
   <svg width="100%" height="100">
-    <path d="M0 ${path}"
+    <path d="M0 ${data.path}"
     stroke="black" stroke-width="2" fill="none"/>
+    ${data.numberList}
   </svg>
   `;
   return graphic;
