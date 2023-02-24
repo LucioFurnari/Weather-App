caf60ee1c064f7711c75e8c6b613036ddce9f30e
LucioFurnari <luciofurnari99@gmail.com>
2023-02-23 21:07:49 -0300

    Create function to draw svg path

--- src/components/uiComponents/graphicComponent.js
+++ src/components/uiComponents/graphicComponent.js
@@ -1,8 +1,25 @@
+const array = [27, 32, 38, 21, 27, 25, 24, 26];
+function createLines(arr) {
+  let path = '';
+  const point = 100;
+  let line = 50;
+  arr.forEach((elem) => {
+    const temp = point - elem;
+    path += `${temp} L ${line} `;
+    line += 50;
+  });
+  return path;
+}
+console.log(createLines(array));
+
 function drawSvgGraphic() {
+  const path = createLines(array);
   const graphic = document.createElement('div');
+  graphic.classList.add('row');
   graphic.innerHTML = `
   <svg width="100%" height="100">
-    <path d="M0 27 H 90 L 180 38 " stroke="black" fill="none"/>
+    <path d="M0 ${path}"
+    stroke="black" stroke-width="2" fill="none"/>
   </svg>
   `;
   return graphic;
