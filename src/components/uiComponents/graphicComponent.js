8ed1e77e414104c59938bd69083bad024b80116a
LucioFurnari <luciofurnari99@gmail.com>
2023-02-24 23:44:28 -0300

    Fix comas in circles tags

--- src/components/uiComponents/graphicComponent.js
+++ src/components/uiComponents/graphicComponent.js
@@ -2,18 +2,20 @@ const array = [27, 32, 38, 21, 27, 25, 24, 26];
 function createLines(arr) {
   let path = '';
   const point = 100;
-  const numberList = [];
+  let numberList = [];
   let line = 50;
   let textLine = 0;
   arr.forEach((elem) => {
     const temp = point - elem;
     const text = `<text x=${textLine} y=${temp}>${elem}</text>`;
     const circle = `<circle cx=${textLine} cy=${temp} r="4" fill="red"/>`;
-    numberList.push(text, circle);
+    numberList.push(text);
+    numberList.push(circle);
     path += `${temp} L ${line} `;
     line += 50;
     textLine += 50;
   });
+  numberList = numberList.join(' ');
   return { path, numberList };
 }
 console.log(createLines(array));
