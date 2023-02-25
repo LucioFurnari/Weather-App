6158f00da0e3400422c0706c099cdcf682e3e92a
LucioFurnari <luciofurnari99@gmail.com>
2023-02-25 17:21:57 -0300

    Change the name of the graphic container

--- src/components/uiComponents/graphicComponent.js
+++ src/components/uiComponents/graphicComponent.js
@@ -2,36 +2,44 @@ const array = [27, 32, 38, 21, 27, 25, 24, 26];
 function createLines(arr) {
   let path = '';
   const point = 100;
-  let numberList = [];
+  const numberList = [];
   let line = 50;
   let textLine = 0;
   arr.forEach((elem) => {
     const temp = point - elem;
     const text = `<text x=${textLine} y=${temp}>${elem}</text>`;
-    const circle = `<circle cx=${textLine} cy=${temp} r="4" fill="red"/>`;
+    const circle = document.createElement('circle');
+    circle.setAttribute('cx', textLine);
+    circle.setAttribute('cy', temp);
+    circle.setAttribute('r', 4);
+    circle.setAttribute('fill', 'red');
+    // const circle = `<circle cx=${textLine} cy=${temp} r="4" fill="red"/>`;
+    circle.addEventListener('mouseover', (event) => {
+      event.target.setAttribute('r', 10);
+    });
     numberList.push(text);
     numberList.push(circle);
     path += `${temp} L ${line} `;
     line += 50;
     textLine += 50;
   });
-  numberList = numberList.join(' ');
+  // numberList = numberList.join(' ');
   return { path, numberList };
 }
-console.log(createLines(array));
 
 function drawSvgGraphic() {
   const data = createLines(array);
-  const graphic = document.createElement('div');
-  graphic.classList.add('row');
-  graphic.innerHTML = `
+  console.log(data);
+  const graphicContainer = document.createElement('div');
+  graphicContainer.classList.add('row');
+  graphicContainer.innerHTML = `
   <svg width="100%" height="100">
     <path d="M0 ${data.path}"
     stroke="black" stroke-width="2" fill="none"/>
     ${data.numberList}
   </svg>
   `;
-  return graphic;
+  return graphicContainer;
 }
 
 export default drawSvgGraphic;
