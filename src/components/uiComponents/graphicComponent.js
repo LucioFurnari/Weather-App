2fd17eeaef665b523997173ae95e9254f1bbe543
LucioFurnari <luciofurnari99@gmail.com>
2023-03-01 23:45:27 -0300

    Add a bottom line to the each hour

--- src/components/uiComponents/graphicComponent.js
+++ src/components/uiComponents/graphicComponent.js
@@ -1,21 +1,36 @@
+function createHoursLine(width) {
+  const tinyLine = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  tinyLine.setAttribute('d', `M${width} 400 L ${width} 380`);
+  return tinyLine;
+}
 function createLines(arr, element) {
   const pathVector = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-  element.append(pathVector);
+  const hLine = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  const tinyLinesContainer = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+  hLine.setAttribute('d', 'M0 400 L 1210 400');
   let path = 'M10';
-  const point = 200;
+  const svgHeight = 400;
   let line = 50;
   let textLine = 10;
+  element.append(pathVector);
   arr.forEach((elem) => {
-    let temp = point - parseInt(elem, 10);
-    if (elem > 30 && elem < 35) {
-      temp -= 40;
+    let temp = svgHeight - parseInt(elem, 10);
+    if (elem > 10 && elem < 15) {
+      temp -= 30;
       path += ` ${temp} L ${line - 40} `;
-    } else if (elem >= 35) {
+    } else if (elem > 20 && elem <= 30) {
       temp -= 60;
       path += ` ${temp} L ${line - 40} `;
+    } else if (elem > 30 && elem < 35) {
+      temp -= 100;
+      path += ` ${temp} L ${line - 40} `;
+    } else if (elem >= 35) {
+      temp -= 120;
+      path += ` ${temp} L ${line - 40} `;
     } else {
-      temp = point - parseInt(elem, 10);
+      temp = svgHeight - parseInt(elem, 10);
     }
+    tinyLinesContainer.append(createHoursLine(textLine));
     path += ` ${temp} L ${line + 10} `;
     line += 50;
     const textVector = document.createElementNS('http://www.w3.org/2000/svg', 'text');
@@ -33,7 +48,7 @@ function createLines(arr, element) {
     circle.addEventListener('mouseleave', (event) => {
       event.target.setAttribute('r', 4);
     });
-    element.append(textVector, circle);
+    element.append(textVector, circle, hLine, tinyLinesContainer);
     textLine += 50;
   });
   pathVector.setAttribute('d', `${path}`);
@@ -43,7 +58,7 @@ function createLines(arr, element) {
 function drawSvgGraphic(arr) {
   const svgGraphic = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
   // svgGraphic.setAttribute('viewBox', '0 0 100 100');
-  svgGraphic.setAttribute('height', 200);
+  svgGraphic.setAttribute('height', 400);
   svgGraphic.setAttribute('width', 1200);
   createLines(arr, svgGraphic);
   const graphicContainer = document.createElement('div');
