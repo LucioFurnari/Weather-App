8ba9e1830034e3d192961dc5f64caf35f60b9140
LucioFurnari <luciofurnari99@gmail.com>
2023-03-05 23:25:49 -0300

    Import search icon svg

--- src/components/uiComponents/formComponent.js
+++ src/components/uiComponents/formComponent.js
@@ -1,12 +1,16 @@
+import SearchIcon from '../../images/magnify.svg';
+
 function formComponent() {
   const cityForm = document.createElement('form');
   const cityInput = document.createElement('input');
   const submitButton = document.createElement('button');
+  const buttonImage = document.createElement('img');
 
-  cityForm.classList.add('card-form');
+  cityForm.classList.add('card-form', 'row');
   cityInput.classList.add('card-input');
   cityInput.required = 'true';
-  submitButton.textContent = 'Submit';
+  submitButton.append(buttonImage);
+  buttonImage.src = SearchIcon;
   cityForm.append(cityInput, submitButton);
 
   return cityForm;
