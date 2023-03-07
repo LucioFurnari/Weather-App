7a545f854d37c45807f6bfd0f4944886fc1dbd9c
LucioFurnari <luciofurnari99@gmail.com>
2023-03-07 17:47:02 -0300

    Remove img from the button

--- src/components/uiComponents/formComponent.js
+++ src/components/uiComponents/formComponent.js
@@ -1,19 +1,15 @@
-import SearchIcon from '../../images/magnify.svg';
-
 function formComponent() {
   const cityForm = document.createElement('form');
   const cityInput = document.createElement('input');
   const messageSpan = document.createElement('span');
   const submitButton = document.createElement('button');
-  const buttonImage = document.createElement('img');
 
-  cityForm.classList.add('city-form');
+  cityForm.classList.add('city-form', 'row');
   cityInput.classList.add('form-input');
   cityInput.placeholder = 'City name';
   cityInput.required = 'true';
   messageSpan.classList.add('form-message');
-  submitButton.append(buttonImage);
-  buttonImage.src = SearchIcon;
+  submitButton.classList.add('form-button');
   cityForm.append(cityInput, messageSpan, submitButton);
 
   return cityForm;
