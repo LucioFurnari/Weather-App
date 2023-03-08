7a4d94c6b97ed3c1f6d07c752ee7d7d762c5df25
LucioFurnari <luciofurnari99@gmail.com>
2023-03-08 18:55:42 -0300

    Create change scale button

--- src/components/mainSection.js
+++ src/components/mainSection.js
@@ -3,11 +3,14 @@ import formComponent from './uiComponents/formComponent';
 function mainSection() {
   const root = document.querySelector('#root');
   const nav = document.createElement('nav');
+  const changeTemperature = document.createElement('button');
   const main = document.createElement('main');
   const form = formComponent();
 
   nav.classList.add('row');
-  nav.append(form);
+  changeTemperature.classList.add('change-button');
+  changeTemperature.textContent = '°F';
+  nav.append(form, changeTemperature);
   root.append(nav, main);
 }
 
